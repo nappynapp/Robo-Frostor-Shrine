0c0fc20e78897e95480f12b6ed9142c8e78fca1c
nappynapp <joanneje05@gmail.com>
2026-09-15 21:07:02 -0500

    fbsdgf

--- assignment2iteration/7-nested-checks/sketh.js
+++ assignment2iteration/7-nested-checks/sketh.js
@@ -1,8 +1,25 @@
 function setup() {
-    createCavas(400, 400)
-    background (220)
-}
-
-function draw() {
-    background(220)
-}
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  
+    let gridSize=10 //columns and rows
+    let squareSize=width/gridSize
+    let spacing=squareSize/2
+  
+    for(let y=0; y<height; y=y+squareSize){
+      for(let x=0; x<width; x=x+squareSize){
+  
+        if (x/squareSize%2==0) {
+        fill(0)
+      } else {
+        fill(255)
+      }
+        
+        rect(x,y,squareSize)
+        // add "+ (spacing * x or y / circleSize)" to space out circles
+      }
+    }
+  }
